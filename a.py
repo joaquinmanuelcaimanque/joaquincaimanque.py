37170d7d3698fb0e392e73a710aa2760da0dc988
Joaquincaimanque <joaquinmanuel.caimanque@gmail.com>
2026-04-24 16:45:57 -0400

    mas tareas

--- a.py
+++ a.py
@@ -1,1 +1,26 @@
-print("hola mundo")
+#definir 2 candidatos
+
+Cand1=(input("ingrese nombre del candidato 1"))
+Cand2=(input("ingrese nombre del candidato 2"))
+
+#preguntar la cantidad d votantes
+
+Cant=(int(input("ingrese la cantidad de votantes")))
+
+v_Cand1=0
+v_Cand2=0
+
+voto=int(input(F"ingrese por quien votara {Cand1} o el {Cand2}"))
+
+if voto==1:
+    v_Cand1=v_Cand1+1
+elif voto==2:
+    v_Cand2+v_Cand2+1
+else:
+    print("error en el voto")      
+
+if v_Cand1>v_Cand2:
+        print("el ganador es {cand1} con votos {v_cand1}")
+
+elif v_Cand2>v_Cand1:
+     print("el ganador es {cand1} con votos {v_cand2}")
